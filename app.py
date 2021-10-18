ac8bfe590a4d9eb42506bbb5161ac6783063cada
Mikhail Budko <mikhailbudko@Mikhails-MacBook-Pro.local>
2021-10-18 01:17:12 -0400

    heroku push

--- app.py
+++ app.py
@@ -562,7 +562,7 @@ def remove_match(match_id):
                     x = 1
                 co1 = float("{:.2f}".format(random.random() * 2))
                 co2 = float("{:.2f}".format(3.2 - co1))
-                prediction = MatchPredictions(match_id, match_to_remove.match_date, match_to_remove.team1, match_to_remove.team2, match_to_remove.match_link, winner, response.get("Probability_1"), response.get("Probability_2"), x, co1, co2)
+                prediction = MatchPredictions(match_id, match_to_remove["match_date"], match_to_remove["team1_name"], match_to_remove["team2_name"], match_to_remove["match_link"], winner, response.get("Probability_1"), response.get("Probability_2"), x, co1, co2)
                 db.session.add(prediction)
                 db.session.commit()
                 matches_list.remove(match_to_remove)
